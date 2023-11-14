66a3c6da4570bc07171ef8e4703fabebf69f4978
Riley Harper <riley.harper@unc.edu>
2023-11-14 15:18:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 document.getElementById('spinButton').addEventListener('click', function() {
-    var result = Math.floor(Math.random() * 3) + 1; // Generates a random number between 1 and 3
-    var displayText = '';
+    // Start spinning
+    var slots = [document.getElementById('slot1'), document.getElementById('slot2'), document.getElementById('slot3')];
+    slots.forEach(function(slot) {
+        slot.classList.add('spin');
+    });
 
-    switch(result) {
-        case 1:
-            displayText = 'Builder';
-            break;
-        case 2:
-            displayText = 'Representative';
-            break;
-        case 3:
-            displayText = 'Distorter';
-            break;
-    }
+    // Calculate result after a delay
+    setTimeout(function() {
+        var result = Math.floor(Math.random() * 3) + 1;
+        var displayText = '';
 
-    document.getElementById('result').innerText = displayText;
+        switch(result) {
+            case 1:
+                displayText = 'Builder - Time to construct your escape!';
+                break;
+            case 2:
+                displayText = 'Representative - Be the face of your team\'s escape!';
+                break;
+            case 3:
+                displayText = 'Distorter - Twist and turn your way out!';
+                break;
+        }
+
+        // Stop spinning and show result
+        slots.forEach(function(slot, index) {
+            slot.classList.remove('spin');
+            slot.textContent = result;
+        });
+
+        document.getElementById('result').innerText = displayText;
+    }, 2000); // Adjust the timeout to match the spin animation duration
 });

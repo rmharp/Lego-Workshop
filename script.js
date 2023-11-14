c0764deece39bda7ae4eb35184e3e1d55a4ee6e0
Riley Harper <riley.harper@unc.edu>
2023-11-14 16:29:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,18 @@
 document.getElementById('spinButton').addEventListener('click', function() {
-    var slots = [document.getElementById('slot1'), document.getElementById('slot2'), document.getElementById('slot3')];
-    slots.forEach(function(slot) {
-        slot.classList.add('spin');
-    });
+    var result = Math.floor(Math.random() * 3) + 1;
+    var displayText = '';
 
-    setTimeout(function() {
-        var result = getWeightedResult();
-        var displayText = '';
-
-        switch(result) {
-            case 1:
-                displayText = 'Builder - Time to construct your escape!';
-                break;
-            case 2:
-                displayText = 'Representative - Be the face of your team\'s escape!';
-                break;
-            case 3:
-                displayText = 'Distorter - Twist and turn your way out!';
-                break;
-        }
-
-        slots.forEach(function(slot) {
-            slot.classList.remove('spin');
-            slot.textContent = result;
-        });
-
-        document.getElementById('result').innerText = displayText;
-    }, 2000);
-});
-
-function getWeightedResult() {
-    var weights = {
-        1: 45, // Builder
-        2: 35, // Representative
-        3: 20  // Distorter
-    };
-
-    var totalWeight = 100;
-    var random = Math.random() * totalWeight;
-    var weightSum = 0;
-
-    for (var i in weights) {
-        weightSum += weights[i];
-        if (random <= weightSum) {
-            return parseInt(i);
-        }
+    switch(result) {
+        case 1:
+            displayText = 'Builder - Time to construct your escape!';
+            break;
+        case 2:
+            displayText = 'Representative - Be the face of your team\'s escape!';
+            break;
+        case 3:
+            displayText = 'Distorter - Twist and turn your way out!';
+            break;
     }
-}
+
+    document.getElementById('result').innerText = displayText;
+});
